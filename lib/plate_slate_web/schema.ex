cdcded48db03ef92ed527a33447666190e65e8ec
Igor Ladkin <dgkplan@googlemail.com>
2017-11-15 22:28:02 +0100

    Add addedOn to the filter

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -13,7 +13,7 @@ defmodule PlateSlateWeb.Schema do
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
-      arg :filter, :menu_item_filter
+      arg :filter, non_null(:menu_item_filter)
       arg :order, type: :sort_order, default_value: :asc
       resolve &Resolvers.Menu.menu_items/3
     end
@@ -28,7 +28,10 @@ defmodule PlateSlateWeb.Schema do
     field :id, :id
     field :name, :string
     field :description, :string
+    field :added_on, :date
   end
+
+  # INPUT OBJECTS
 
   @desc "Filtering options for the menu item list"
   input_object :menu_item_filter do
@@ -46,5 +49,28 @@ defmodule PlateSlateWeb.Schema do
 
     @desc "Priced below a value"
     field :priced_below, :float
+
+    @desc "Added to the menu before this date"
+    field :added_before, :date
+
+    @desc "Added to the menu after this date"
+    field :added_after, :date
+  end
+
+  # SCALARS
+
+  scalar :date do
+    parse fn input ->
+      with %Absinthe.Blueprint.Input.String{value: value} <- input,
+      {:ok, date} <- Date.from_iso8601(value) do
+        {:ok, date}
+      else
+        _ -> :error
+      end
+    end
+
+    serialize fn date ->
+      Date.to_iso8601(date)
+    end
   end
 end
--- test/plate_slate_web/schema/query/menu_item_test.exs
+++ test/plate_slate_web/schema/query/menu_item_test.exs
@@ -7,7 +7,7 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
 
   @query """
   {
-    menuItems {
+    menuItems(filter: {}) {
       name
     }
   }
@@ -38,7 +38,7 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
 
   @query """
   query ($order: SortOrder!) {
-    menuItems(order: $order) {
+    menuItems(order: $order, filter: {}) {
       name
     }
   }
@@ -78,6 +78,32 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
     response = get build_conn(), "/api", query: @query, variables: @variables
     assert %{
       "data" => %{"menuItems" => [%{"name" => "Vada Pav"}]}
+    } == json_response(response, 200)
+  end
+
+  @query """
+  query ($filter: MenuItemFilter!) {
+    menuItems(filter: $filter) {
+      name
+      addedOn
+    }
+  }
+  """
+  @variables %{filter: %{"addedBefore" => "2017-01-20", "category" => "Sides"}}
+  test "menuItems filtered by date custom scalar" do
+    sides = PlateSlate.Repo.get_by!(PlateSlate.Menu.Category, name: "Sides")
+    %PlateSlate.Menu.Item{
+      name: "Garlic Fries",
+      added_on: ~D[2017-01-01],
+      price: 2.50,
+      category: sides
+    } |> PlateSlate.Repo.insert!()
+
+    response = get build_conn(), "/api", query: @query, variables: @variables
+    assert %{
+      "data" => %{
+        "menuItems" => [%{"name" => "Garlic Fries", "addedOn" => "2017-01-01"}]
+      }
     } == json_response(response, 200)
   end
 end
