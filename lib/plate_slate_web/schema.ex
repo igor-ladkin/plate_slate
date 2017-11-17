1984eaae5c802269f4c8ae0ed1ea7eac44ced222
Igor Ladkin <igor.ladkin@xing.com>
2017-11-17 12:59:44 +0100

    Add search via item names and categories

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -10,6 +10,8 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
   alias PlateSlateWeb.Resolvers
 
+  import_types __MODULE__.MenuTypes
+
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
@@ -17,44 +19,18 @@ defmodule PlateSlateWeb.Schema do
       arg :order, type: :sort_order, default_value: :asc
       resolve &Resolvers.Menu.menu_items/3
     end
+
+    field :search, list_of(:search_result) do
+      arg :matching, non_null(:string)
+      resolve &Resolvers.Menu.search/3
+    end
   end
+
+  # ENUMS
 
   enum :sort_order do
     value :asc
     value :desc
-  end
-
-  object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
-    field :added_on, :date
-  end
-
-  # INPUT OBJECTS
-
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
-    field :name, :string
-
-    @desc "Matching a category name"
-    field :category, :string
-
-    @desc "Matching a tag"
-    field :tag, :string
-
-    @desc "Priced above a value"
-    field :priced_above, :float
-
-    @desc "Priced below a value"
-    field :priced_below, :float
-
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-
-    @desc "Added to the menu after this date"
-    field :added_after, :date
   end
 
   # SCALARS
--- test/plate_slate_web/schema/query/menu_item_test.exs
+++ test/plate_slate_web/schema/query/menu_item_test.exs
@@ -106,4 +106,22 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
       }
     } == json_response(response, 200)
   end
+
+  @query """
+  query Search($term: String!) {
+    search(matching: $term) {
+      ... on MenuItem { name }
+      ... on Category { name }
+      __typename
+    }
+  }
+  """
+  @variables %{term: "e"}
+  test "search returns a list of menu items and categories" do
+    response = get build_conn(), "/api", query: @query, variables: @variables
+    assert %{"data" => %{"search" => results}} = json_response(response, 200)
+    assert length(results) > 0
+    assert Enum.find(results, &(&1["__typename"] == "Category"))
+    assert Enum.find(results, &(&1["__typename"] == "MenuItem"))
+  end
 end
