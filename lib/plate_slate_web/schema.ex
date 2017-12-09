ffcc287ec30784a0baa930698b646f535f9fb8da
Igor Ladkin <igor.ladkin@xing.com>
2017-12-09 13:09:45 +0100

    Subscriptions on order updates

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -29,6 +29,16 @@ defmodule PlateSlateWeb.Schema do
       arg :input, non_null(:place_order_input)
       resolve &Resolvers.Ordering.place_order/3
     end
+
+    field :ready_order, :order_result do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Ordering.ready_order/3
+    end
+
+    field :complete_order, :order_result do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Ordering.complete_order/3
+    end
   end
 
   subscription do
@@ -40,6 +50,23 @@ defmodule PlateSlateWeb.Schema do
       resolve fn root, _, _ ->
         IO.inspect(root)
         {:ok, root}
+      end
+    end
+
+    field :update_order, :order do
+      arg :id, non_null(:id)
+
+      config fn args, _info ->
+        {:ok, topic: args.id}
+      end
+
+      trigger [:ready_order, :complete_order], topic: fn
+        %{order: order} -> [to_string(order.id)]
+        _ -> []
+      end
+
+      resolve fn %{order: order}, _, _ ->
+        {:ok, order}
       end
     end
   end
